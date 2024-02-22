c2a6778761deca717b4ea22cc94280bfd4dc4f17
Justino Felipe <jf_lopesnunes@hotmail.com>
2024-02-22 00:44:19 -0300

    Literais strings

--- jackcompiler-master/src/test/java/br/ufma/ecp/CodeGenerator.java
+++ jackcompiler-master/src/test/java/br/ufma/ecp/CodeGenerator.java
@@ -44,5 +44,45 @@ public class CodeGenerator extends TestSupport {
                     """;
             assertEquals(expected, actual);
     }
+
+    @Test
+    public void testSimpleExpression2 () {
+        var input = """
+            10 + 30 * 40
+            """;
+        
+        var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+        parser.parseExpression();
+        String actual = parser.VMOutput();
+        String expected = """
+                push constant 10
+                push constant 30
+                add
+                push constant 40
+                call Math.multiply 2       
+                    """;
+            assertEquals(expected, actual);
+    }
     
+    @Test
+    public void testLiteralString () {
+        var input = """
+            "OLA"
+            """;
+        
+        var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+        parser.parseExpression();
+        String actual = parser.VMOutput();
+        String expected = """
+                push constant 3
+                call String.new 1
+                push constant 79
+                call String.appendChar 2
+                push constant 76
+                call String.appendChar 2
+                push constant 65
+                call String.appendChar 2
+                    """;
+            assertEquals(expected, actual);
+    }
 }
